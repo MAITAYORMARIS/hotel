b5c87e26e008ecff60ecf49255b96695ec63f820
MAITAYORMARIS <yormarism@gmail.com>
2023-12-04 20:13:30 -0300

    ajustando carpetas

--- archivosJS/restaurante.js
+++ archivosJS/restaurante.js
@@ -1,3 +1,25 @@
+let dataRestaurante = []
+
+async function getData() {
+    let dataApi
+    let menu
+    await fetch("https://yormaris.pythonanywhere.com/menu")
+        // .then(console.log(response))
+        .then(response => response.json())
+
+        .then(json => dataApi = json)
+
+
+    menu = dataApi
+    console.log(menu)
+    for (var i = 0; i < menu.length; i++) {
+        dataRestaurante.push(menu[i])
+
+    }
+    construirMenu(dataRestaurante)
+} getData()
+
+
 document.addEventListener("DOMContentLoaded", function () {
 
     const anteriorImagenBtn = document.getElementById("anteriorImagen");
@@ -34,3 +56,50 @@ document.addEventListener("DOMContentLoaded", function () {
 
 });
 
+function construirMenu(array) {
+    var html = "";
+    for (var i = 0; i < array.length; i++) {
+        if (i % 2 === 0) {
+            // si la posicion es par la imagen aparece a la izquierda
+            html +=
+                `
+          <div class="productosItem ">
+            <div class="productosPortada zoom">
+              <img src=${array[i].foto} alt="foto"+${array[i].nombre}>
+            </div>
+            <div class="productoInfo">
+              <h3>${array[i].nombre}</h3>
+              <p>${array[i].descripcion}</p>
+              <p><i class="fa-solid fa-circle-info"></i>Categoria: ${array[i].categoria}</p>
+              
+                <div class="detalleDerecha">     
+                  <h3>Precio USD: ${array[i].precio}</h3>
+                </div>
+            </div>
+          </div>`
+        }
+        // si el indice es impar la imagen aparece a la derecha y el texto a la izquierda
+        else {
+            html +=
+                `
+          <div class="productosItem fondo ">
+            <div class="productoInfo">
+              <h3>${array[i].nombre}</h3>
+              <p>${array[i].descripcion}</p>
+              <p><i class="fa-solid fa-circle-info"></i>Categoria: ${array[i].categoria}</p>
+              
+              <div class="detalleDerecha">     
+                <h3>Precio USD: ${array[i].precio}</h3>
+              </div>
+            </div>                
+                        
+            
+            <div class="productosPortada zoom">
+            <img src=${array[i].foto} alt="foto"+${array[i].nombre}>
+            </div>
+            </div>`
+        }
+
+    }
+    document.getElementById('contenedorMenu').innerHTML = html;
+}
